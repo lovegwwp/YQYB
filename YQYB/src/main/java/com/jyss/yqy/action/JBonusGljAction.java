bf1954b02663ad9b70d8e385f607170f8942f65b
lovegwwp <435217379@qq.com>
2017-12-21 09:47:59 +0800

    page=rows

--- YQYB/src/main/java/com/jyss/yqy/action/JBonusGljAction.java
+++ YQYB/src/main/java/com/jyss/yqy/action/JBonusGljAction.java
@@ -1,63 +1,140 @@
 package com.jyss.yqy.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jyss.yqy.entity.JBonusGljResult;
+import com.jyss.yqy.entity.Page;
 import com.jyss.yqy.service.JBonusGljService;
 
 @Controller
-@RequestMapping("/showGlj")
 public class JBonusGljAction {
-	
+
 	@Autowired
 	private JBonusGljService jBonusGljService;
-	
-	
+
 	/**
 	 * 昨日总金额
 	 */
-	@RequestMapping("/list")
-	@ResponseBody
-	public JBonusGljResult selectGljTotal(){
-		JBonusGljResult result = jBonusGljService.selectGljTotal();
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showGlj/list")
+	// @ResponseBody
+	// public JBonusGljResult selectGljTotal(){
+	// JBonusGljResult result = jBonusGljService.selectGljTotal();
+	// return result;
+	// }
+
 	/**
 	 * 本周总金额
 	 */
-	@RequestMapping("/listByWek")
-	@ResponseBody
-	public JBonusGljResult selectGljTotalByWek(){
-		JBonusGljResult result = jBonusGljService.selectGljTotalByWek();
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showGlj/listByWek")
+	// @ResponseBody
+	// public JBonusGljResult selectGljTotalByWek(){
+	// JBonusGljResult result = jBonusGljService.selectGljTotalByWek();
+	// return result;
+	// }
+
 	/**
 	 * 两个日期查询总收益
 	 */
-	@RequestMapping("/listByDay")
-	@ResponseBody
-	public JBonusGljResult selectGljTotalByDay(@RequestParam("beginTime")String beginTime,@RequestParam("endTime")String endTime){
-		JBonusGljResult result = jBonusGljService.selectGljTotalByDay(beginTime, endTime);
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showGlj/listByDay")
+	// @ResponseBody
+	// public JBonusGljResult
+	// selectGljTotalByDay(@RequestParam("beginTime")String
+	// beginTime,@RequestParam("endTime")String endTime){
+	// JBonusGljResult result = jBonusGljService.selectGljTotalByDay(beginTime,
+	// endTime);
+	// return result;
+	// }
+
 	/**
 	 * 按月查询总收益
 	 */
-	@RequestMapping("/listByMonth")
+	// @RequestMapping("/showGlj/listByMonth")
+	// @ResponseBody
+	// public JBonusGljResult selectGljTotalByMonth(@RequestParam("month")String
+	// month){
+	// JBonusGljResult result = jBonusGljService.selectGljTotalByMonth(month);
+	// return result;
+	// }
+
+	/**
+	 * 昨日总金额
+	 */
+	@RequestMapping("/showGlj/list")
 	@ResponseBody
-	public JBonusGljResult selectGljTotalByMonth(@RequestParam("month")String month){
+	public Page<JBonusGljResult> selectGljTotal(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusGljResult> list = new ArrayList<JBonusGljResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusGljResult result = jBonusGljService.selectGljTotal();
+		list.add(result);
+		PageInfo<JBonusGljResult> pageInfoBy = new PageInfo<JBonusGljResult>(
+				list);
+		return new Page<JBonusGljResult>(pageInfoBy);
+	}
+
+	/**
+	 * 本周总金额
+	 */
+	@RequestMapping("/showGlj/listByWek")
+	@ResponseBody
+	public Page<JBonusGljResult> selectGljTotalByWek(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusGljResult> list = new ArrayList<JBonusGljResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusGljResult result = jBonusGljService.selectGljTotalByWek();
+		list.add(result);
+		PageInfo<JBonusGljResult> pageInfoBy = new PageInfo<JBonusGljResult>(
+				list);
+		return new Page<JBonusGljResult>(pageInfoBy);
+	}
+
+	/**
+	 * 两个日期查询总收益
+	 */
+	@RequestMapping("/showGlj/listByDay")
+	@ResponseBody
+	public Page<JBonusGljResult> selectGljTotalByDay(
+			@RequestParam("beginTime") String beginTime,
+			@RequestParam("endTime") String endTime,
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusGljResult> list = new ArrayList<JBonusGljResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusGljResult result = jBonusGljService.selectGljTotalByDay(
+				beginTime, endTime);
+		list.add(result);
+		PageInfo<JBonusGljResult> pageInfoBy = new PageInfo<JBonusGljResult>(
+				list);
+		return new Page<JBonusGljResult>(pageInfoBy);
+	}
+
+	/**
+	 * 按月查询总收益
+	 */
+	@RequestMapping("/showGlj/listByMonth")
+	@ResponseBody
+	public Page<JBonusGljResult> selectGljTotalByMonth(
+			@RequestParam("month") String month,
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusGljResult> list = new ArrayList<JBonusGljResult>();
+		PageHelper.startPage(page, rows);// 分页语句
 		JBonusGljResult result = jBonusGljService.selectGljTotalByMonth(month);
-		return result;
+		list.add(result);
+		PageInfo<JBonusGljResult> pageInfoBy = new PageInfo<JBonusGljResult>(
+				list);
+		return new Page<JBonusGljResult>(pageInfoBy);
 	}
 
 }
--- YQYB/src/main/java/com/jyss/yqy/action/JBonusScjAction.java
+++ YQYB/src/main/java/com/jyss/yqy/action/JBonusScjAction.java
@@ -1,71 +1,151 @@
 package com.jyss.yqy.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jyss.yqy.entity.JBonusScjResult;
+import com.jyss.yqy.entity.Page;
 import com.jyss.yqy.service.JBonusScjService;
 
 @Controller
-@RequestMapping("/showScj")
 public class JBonusScjAction {
-	
+
 	@Autowired
 	private JBonusScjService bonusScjService;
-	
-	
+
 	/**
 	 * 昨日列表
 	 */
-	@RequestMapping("/list")
-	@ResponseBody
-	public JBonusScjResult selectJBonusScj(@RequestParam(value = "page", required = true) int page,
-			@RequestParam(value = "limit", required = true) int limit){
-		JBonusScjResult result = bonusScjService.selectJBonusScj(page, limit);
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showScj/list")
+	// @ResponseBody
+	// public JBonusScjResult selectJBonusScj(
+	// @RequestParam(value = "page", required = true) int page,
+	// @RequestParam(value = "rows", required = true) int rows) {
+	// JBonusScjResult result = bonusScjService.selectJBonusScj(page, rows);
+	// return result;
+	// }
+
 	/**
 	 * 本周列表
 	 */
-	@RequestMapping("/listByWek")
-	@ResponseBody
-	public JBonusScjResult selectJBonusScjWek(@RequestParam(value = "page", required = true) int page,
-			@RequestParam(value = "limit", required = true) int limit){
-		JBonusScjResult result = bonusScjService.selectJBonusScjWek(page, limit);
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showScj/listByWek")
+	// @ResponseBody
+	// public JBonusScjResult selectJBonusScjWek(
+	// @RequestParam(value = "page", required = true) int page,
+	// @RequestParam(value = "rows", required = true) int rows) {
+	// JBonusScjResult result = bonusScjService
+	// .selectJBonusScjWek(page, rows);
+	// return result;
+	// }
+
 	/**
 	 * 两个日期查询个人列表总值
 	 */
-	@RequestMapping("/listByDay")
-	@ResponseBody
-	public JBonusScjResult selectJBonusScjByDay(@RequestParam(value = "page", required = true) int page,
-			@RequestParam(value = "limit", required = true) int limit,
-			@RequestParam("beginTime")String beginTime,@RequestParam("endTime")String endTime){
-		JBonusScjResult result = bonusScjService.selectJBonusScjByDay(page, limit, beginTime, endTime);
-		return result;
-	}
-	
-	
+	// @RequestMapping("/showScj/listByDay")
+	// @ResponseBody
+	// public JBonusScjResult selectJBonusScjByDay(
+	// @RequestParam(value = "page", required = true) int page,
+	// @RequestParam(value = "rows", required = true) int rows,
+	// @RequestParam("beginTime") String beginTime,
+	// @RequestParam("endTime") String endTime) {
+	// JBonusScjResult result = bonusScjService.selectJBonusScjByDay(page,
+	// rows, beginTime, endTime);
+	// return result;
+	// }
+
 	/**
 	 * 按月查询查询个人列表总值
 	 */
-	@RequestMapping("/listByMonth")
+	// @RequestMapping("/showScj/listByMonth")
+	// @ResponseBody
+	// public JBonusScjResult selectJBonusScjByMonth(
+	// @RequestParam(value = "page", required = true) int page,
+	// @RequestParam(value = "rows", required = true) int rows,
+	// @RequestParam("month") String month) {
+	// JBonusScjResult result = bonusScjService.selectJBonusScjByMonth(page,
+	// rows, month);
+	// return result;
+	// }
+
+	/**
+	 * 昨日列表
+	 */
+	@RequestMapping("/showScj/list")
 	@ResponseBody
-	public JBonusScjResult selectJBonusScjByMonth(@RequestParam(value = "page", required = true) int page,
-			@RequestParam(value = "limit", required = true) int limit,
-			@RequestParam("month") String month){
-		JBonusScjResult result = bonusScjService.selectJBonusScjByMonth(page, limit, month);
-		return result;
+	public Page<JBonusScjResult> selectJBonusScj(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusScjResult> list = new ArrayList<JBonusScjResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusScjResult result = bonusScjService.selectJBonusScj(page, rows);
+		list.add(result);
+		PageInfo<JBonusScjResult> pageInfoBy = new PageInfo<JBonusScjResult>(
+				list);
+		return new Page<JBonusScjResult>(pageInfoBy);
 	}
-	
+
+	/**
+	 * 本周列表
+	 */
+	@RequestMapping("/showScj/listByWek")
+	@ResponseBody
+	public Page<JBonusScjResult> selectJBonusScjWek(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows) {
+		List<JBonusScjResult> list = new ArrayList<JBonusScjResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusScjResult result = bonusScjService.selectJBonusScjWek(page, rows);
+		list.add(result);
+		PageInfo<JBonusScjResult> pageInfoBy = new PageInfo<JBonusScjResult>(
+				list);
+		return new Page<JBonusScjResult>(pageInfoBy);
+	}
+
+	/**
+	 * 两个日期查询个人列表总值
+	 */
+	@RequestMapping("/showScj/listByDay")
+	@ResponseBody
+	public Page<JBonusScjResult> selectJBonusScjByDay(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows,
+			@RequestParam("beginTime") String beginTime,
+			@RequestParam("endTime") String endTime) {
+		List<JBonusScjResult> list = new ArrayList<JBonusScjResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusScjResult result = bonusScjService.selectJBonusScjByDay(page,
+				rows, beginTime, endTime);
+		list.add(result);
+		PageInfo<JBonusScjResult> pageInfoBy = new PageInfo<JBonusScjResult>(
+				list);
+		return new Page<JBonusScjResult>(pageInfoBy);
+	}
+
+	/**
+	 * 按月查询查询个人列表总值
+	 */
+	@RequestMapping("/showScj/listByMonth")
+	@ResponseBody
+	public Page<JBonusScjResult> selectJBonusScjByMonth(
+			@RequestParam(value = "page", required = true) int page,
+			@RequestParam(value = "rows", required = true) int rows,
+			@RequestParam("month") String month) {
+		List<JBonusScjResult> list = new ArrayList<JBonusScjResult>();
+		PageHelper.startPage(page, rows);// 分页语句
+		JBonusScjResult result = bonusScjService.selectJBonusScjByMonth(page,
+				rows, month);
+		list.add(result);
+		PageInfo<JBonusScjResult> pageInfoBy = new PageInfo<JBonusScjResult>(
+				list);
+		return new Page<JBonusScjResult>(pageInfoBy);
+	}
 
 }
