7c4ff526c570e2c82f0e3b08a09e8a407faea22f
sosahh <1447961434@qq.com>
2018-02-08 15:34:58 +0800

    修改

--- YQYB/src/main/java/com/jyss/yqy/service/impl/UserRecordBServiceImpl.java
+++ YQYB/src/main/java/com/jyss/yqy/service/impl/UserRecordBServiceImpl.java
@@ -46,145 +46,204 @@ public class UserRecordBServiceImpl implements UserRecordBService {
 	@Override
 	public ResponseEntity insertJBonusGlj(String uuid){      
 		List<UserBean> userList = userMapper.getUserByUuid(uuid);
-		UserBean userBean = userList.get(0); // 获取被推荐人信息
+		UserBean userBean = userList.get(0);   // 获取被推荐人信息
 		int isAuth = userBean.getIsAuth();
 		int uLevel = userBean.getIsChuangke();
 		if(isAuth == 2 && (uLevel == 2 || uLevel == 3 || uLevel == 4 || uLevel == 5)){
 			int count = userRecordMapper.updateByUid(userBean.getId(), 1);
 			
 			if(count == 1){
-				UUserRRecordBExample example = new UUserRRecordBExample();
-				Criteria criteria = example.createCriteria();
-				criteria.andUIdEqualTo(userBean.getId());
-				criteria.andStatusEqualTo(1);
-				List<UUserRRecordB> list = userRecordMapper.selectByExample(example);
-				if(list != null && list.size()>0){
-					UUserRRecordB userRecord = list.get(0);
-					
-					List<UserBean> nameById = userMapper.getUserNameById(userRecord.getrId());
-					if(nameById != null && nameById.size()>0){
-						UserBean userBean1 = nameById.get(0);
-						int pLevel = userBean1.getIsChuangke();
-						
-						//查询奖励额
-						Xtcl xtcl1 = xtclMapper.getClsValue("glj_type", "1");   //初级获得金额
-						double double1 = Double.parseDouble(xtcl1.getBz_value());    //40.00
-						Xtcl xtcl2 = xtclMapper.getClsValue("glj_type", "2");   //中级获得金额
-						double double2 = Double.parseDouble(xtcl2.getBz_value());    //80.00
-						Xtcl xtcl3 = xtclMapper.getClsValue("glj_type", "3");   //高级获得金额
-						double double3 = Double.parseDouble(xtcl3.getBz_value());    //120.00
-						 
-						
-						JBonusGlj bonusGlj = new JBonusGlj();
-						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-						bonusGlj.setuId(userBean.getId());
-						bonusGlj.setuName(userBean.getRealName());
-						bonusGlj.setParentId(userRecord.getrId());
-						bonusGlj.setStatus(1);
-						bonusGlj.setCreated(sdf.format(new Date()));
-						if (pLevel == 4) {
-							bonusGlj.setParentMoney(double3);
-							bonusGljMapper.insert(bonusGlj);
-							computeJF(userRecord.getrId(),double3);
-							
-						} else if(pLevel == 3) {
-							bonusGlj.setParentMoney(double2);
-							bonusGljMapper.insert(bonusGlj);
-							computeJF(userRecord.getrId(),double2);
-							
-							UUserRRecordBExample example1 = new UUserRRecordBExample();
-							Criteria criteria1 = example1.createCriteria();
-							criteria1.andUIdEqualTo(userRecord.getrId());
-							criteria1.andStatusEqualTo(1);
-							List<UUserRRecordB> list1 = userRecordMapper.selectByExample(example1);
-							if(list1 != null && list1.size()>0){
-								UUserRRecordB userRecord1 = list1.get(0);
-								/*List<UserBean> nameById1 = userMapper.getUserNameById(userRecord1.getrId());
-							if(nameById1 != null && nameById1.size()>0){
-								UserBean userBean2 = nameById1.get(0);*/
-								
-								JBonusGlj bonusGlj1 = new JBonusGlj();
-								bonusGlj1.setuId(userRecord1.getuId());
-								bonusGlj1.setuName(userBean1.getRealName());
-								bonusGlj1.setParentId(userRecord1.getrId());
-								bonusGlj1.setParentMoney(double1);
-								bonusGlj1.setStatus(1);
-								bonusGlj1.setCreated(sdf.format(new Date()));
-								bonusGljMapper.insert(bonusGlj1);
-								computeJF(userRecord1.getrId(),double1);
-								//}
-							}
-						}else if(pLevel == 2){
-							bonusGlj.setParentMoney(double1);
-							bonusGljMapper.insert(bonusGlj);
-							computeJF(userRecord.getrId(),double1);
-							
-							UUserRRecordBExample example1 = new UUserRRecordBExample();
-							Criteria criteria1 = example1.createCriteria();
-							criteria1.andUIdEqualTo(userRecord.getrId());
-							criteria1.andStatusEqualTo(1);
-							List<UUserRRecordB> list1 = userRecordMapper.selectByExample(example1);
-							if(list1 != null && list1.size()>0){
-								UUserRRecordB userRecord1 = list1.get(0);
-								List<UserBean> nameById1 = userMapper.getUserNameById(userRecord1.getrId());
-								if(nameById1 != null && nameById1.size()>0){
-									UserBean userBean2 = nameById1.get(0);
-									int level1 = userBean2.getIsChuangke();
-									if(level1 == 3 || level1 == 4){
-										JBonusGlj bonusGlj1 = new JBonusGlj();
-										bonusGlj1.setuId(userRecord1.getuId());
-										bonusGlj1.setuName(userBean1.getRealName());
-										bonusGlj1.setParentId(userRecord1.getrId());
-										bonusGlj1.setParentMoney(double2);
-										bonusGlj1.setStatus(1);
-										bonusGlj1.setCreated(sdf.format(new Date()));
-										bonusGljMapper.insert(bonusGlj1);
-										computeJF(userRecord1.getrId(),double2);
-									}else if(level1 == 2){
-										JBonusGlj bonusGlj1 = new JBonusGlj();
-										bonusGlj1.setuId(userRecord1.getuId());
-										bonusGlj1.setuName(userBean1.getRealName());
-										bonusGlj1.setParentId(userRecord1.getrId());
-										bonusGlj1.setParentMoney(double1);
-										bonusGlj1.setStatus(1);
-										bonusGlj1.setCreated(sdf.format(new Date()));
-										bonusGljMapper.insert(bonusGlj1);
-										computeJF(userRecord1.getrId(),double1);
-										
-										UUserRRecordBExample example2 = new UUserRRecordBExample();
-										Criteria criteria2 = example2.createCriteria();
-										criteria2.andUIdEqualTo(userRecord1.getrId());
-										criteria2.andStatusEqualTo(1);
-										List<UUserRRecordB> list2 = userRecordMapper.selectByExample(example2);
-										if (list2 != null && list2.size() > 0){
-											UUserRRecordB userRecord2 = list2.get(0);
-											JBonusGlj bonusGlj2 = new JBonusGlj();
-											bonusGlj2.setuId(userRecord2.getuId());
-											bonusGlj2.setuName(userBean2.getRealName());
-											bonusGlj2.setParentId(userRecord2.getrId());
-											bonusGlj2.setParentMoney(double1);
-											bonusGlj2.setStatus(1);
-											bonusGlj2.setCreated(sdf.format(new Date()));
-											bonusGljMapper.insert(bonusGlj2);
-											computeJF(userRecord2.getrId(),double1);
-										}
-									}
-								}
-								
-							}
-						}
-					}
-					return new ResponseEntity("true", "操作成功！");
-				}
+				computeJBonusGlj(userBean.getId());
+
+				return new ResponseEntity("true", "操作成功！");
 			}
 		}
 		return new ResponseEntity("false", "操作失败！");
 		
 	}
-	
-	
+
+
 	/**
-	 * 计算积分
+	 * 计算管理奖
+	 * @param uId
+	 */
+	private void computeJBonusGlj(int uId){
+		//查询关系表
+		UUserRRecordBExample example = new UUserRRecordBExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andUIdEqualTo(uId);
+		criteria.andStatusEqualTo(1);
+		List<UUserRRecordB> list = userRecordMapper.selectByExample(example);
+		if(list != null && list.size()>0){
+			UUserRRecordB userRecord = list.get(0);
+			Integer pLevel = userRecord.getType();    //父级代理级别
+
+			//查询奖励额
+			Xtcl xtcl1 = xtclMapper.getClsValue("glj_type", "1");   //初级获得金额
+			double double1 = Double.parseDouble(xtcl1.getBz_value());            //40.00
+			Xtcl xtcl2 = xtclMapper.getClsValue("glj_type", "2");   //中级获得金额
+			double double2 = Double.parseDouble(xtcl2.getBz_value());            //80.00
+			Xtcl xtcl3 = xtclMapper.getClsValue("glj_type", "3");   //高级获得金额
+			double double3 = Double.parseDouble(xtcl3.getBz_value());            //120.00
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+			//经理人
+			if(pLevel == 5){
+				computeJBonusGlj(userRecord.getrId());
+
+			//高级代理人，结束
+			}else if(pLevel == 4){
+				List<UserBean> userList = userMapper.getUserNameById(userRecord.getuId());
+				if(userList != null && userList.size()>0){
+					UserBean userBean = userList.get(0);
+					JBonusGlj bonusGlj = new JBonusGlj();
+					bonusGlj.setuId(userBean.getId());
+					bonusGlj.setuName(userBean.getRealName());
+					bonusGlj.setParentId(userRecord.getrId());
+					bonusGlj.setStatus(1);
+					bonusGlj.setCreated(sdf.format(new Date()));
+					bonusGlj.setParentMoney(double3);
+					bonusGljMapper.insert(bonusGlj);
+					computeJF(userRecord.getrId(),double3);
+				}
+
+			//中级代理人
+			}else if(pLevel == 3){
+				List<UserBean> userList = userMapper.getUserNameById(userRecord.getuId());
+				if(userList != null && userList.size()>0){
+					UserBean userBean = userList.get(0);
+					JBonusGlj bonusGlj = new JBonusGlj();
+					bonusGlj.setuId(userBean.getId());
+					bonusGlj.setuName(userBean.getRealName());
+					bonusGlj.setParentId(userRecord.getrId());
+					bonusGlj.setStatus(1);
+					bonusGlj.setCreated(sdf.format(new Date()));
+					bonusGlj.setParentMoney(double2);
+					bonusGljMapper.insert(bonusGlj);
+					computeJF(userRecord.getrId(),double2);
+					//递归到高级代理人，结束
+					UUserRRecordB userRecord1 = selectGjDlr(userRecord.getrId(), 4);
+					if(userRecord1 != null){
+						List<UserBean> userList1 = userMapper.getUserNameById(userRecord1.getuId());
+						if(userList1 != null && userList1.size()>0){
+							UserBean userBean1 = userList1.get(0);
+							JBonusGlj bonusGlj1 = new JBonusGlj();
+							bonusGlj1.setuId(userBean1.getId());
+							bonusGlj1.setuName(userBean1.getRealName());
+							bonusGlj1.setParentId(userRecord1.getrId());
+							bonusGlj1.setStatus(1);
+							bonusGlj1.setCreated(sdf.format(new Date()));
+							bonusGlj1.setParentMoney(double3 - double2);
+							bonusGljMapper.insert(bonusGlj1);
+							computeJF(userRecord1.getrId(),double3 - double2);
+						}
+					}
+				}
+
+			//初级代理人
+			}else if(pLevel == 2){
+				List<UserBean> userList = userMapper.getUserNameById(userRecord.getuId());
+				if(userList != null && userList.size()>0){
+					UserBean userBean = userList.get(0);
+					JBonusGlj bonusGlj = new JBonusGlj();
+					bonusGlj.setuId(userBean.getId());
+					bonusGlj.setuName(userBean.getRealName());
+					bonusGlj.setParentId(userRecord.getrId());
+					bonusGlj.setStatus(1);
+					bonusGlj.setCreated(sdf.format(new Date()));
+					bonusGlj.setParentMoney(double1);
+					bonusGljMapper.insert(bonusGlj);
+					computeJF(userRecord.getrId(),double1);
+
+					UUserRRecordB userRecord1 = selectGjDlr(userRecord.getrId(), 3);
+					if(userRecord1 != null){
+						Integer type = userRecord1.getType();
+						//递归到高级代理人，结束
+						if(type == 4){
+							List<UserBean> userList1 = userMapper.getUserNameById(userRecord1.getuId());
+							if(userList1 != null && userList1.size()>0){
+								UserBean userBean1 = userList1.get(0);
+								JBonusGlj bonusGlj1 = new JBonusGlj();
+								bonusGlj1.setuId(userBean1.getId());
+								bonusGlj1.setuName(userBean1.getRealName());
+								bonusGlj1.setParentId(userRecord1.getrId());
+								bonusGlj1.setStatus(1);
+								bonusGlj1.setCreated(sdf.format(new Date()));
+								bonusGlj1.setParentMoney(double3 - double1);
+								bonusGljMapper.insert(bonusGlj1);
+								computeJF(userRecord1.getrId(),double3 - double1);
+							}
+						//递归到中级代理人
+						}else if(type == 3){
+							List<UserBean> userList1 = userMapper.getUserNameById(userRecord1.getuId());
+							if(userList1 != null && userList1.size()>0){
+								UserBean userBean1 = userList1.get(0);
+								JBonusGlj bonusGlj1 = new JBonusGlj();
+								bonusGlj1.setuId(userBean1.getId());
+								bonusGlj1.setuName(userBean1.getRealName());
+								bonusGlj1.setParentId(userRecord1.getrId());
+								bonusGlj1.setStatus(1);
+								bonusGlj1.setCreated(sdf.format(new Date()));
+								bonusGlj1.setParentMoney(double2 - double1);
+								bonusGljMapper.insert(bonusGlj1);
+								computeJF(userRecord1.getrId(),double2 - double1);
+
+								//递归到高级代理人，结束
+								UUserRRecordB userRecord2 = selectGjDlr(userRecord.getrId(), 4);
+								if(userRecord2 != null){
+									List<UserBean> userList2 = userMapper.getUserNameById(userRecord2.getuId());
+									if(userList2 != null && userList2.size()>0){
+										double gjDlr = double3 - (double2 - double1) - double1;
+										UserBean userBean2 = userList2.get(0);
+										JBonusGlj bonusGlj2 = new JBonusGlj();
+										bonusGlj2.setuId(userBean2.getId());
+										bonusGlj2.setuName(userBean2.getRealName());
+										bonusGlj2.setParentId(userRecord2.getrId());
+										bonusGlj2.setStatus(1);
+										bonusGlj2.setCreated(sdf.format(new Date()));
+										bonusGlj2.setParentMoney(gjDlr);
+										bonusGljMapper.insert(bonusGlj2);
+										computeJF(userRecord2.getrId(),gjDlr);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * 递归中高级代理
+	 * @param uId
+	 * @param type
+	 * @return
+	 */
+	private UUserRRecordB selectGjDlr(int uId,int type){
+		UUserRRecordBExample example = new UUserRRecordBExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andUIdEqualTo(uId);
+		criteria.andStatusEqualTo(1);
+		List<UUserRRecordB> list = userRecordMapper.selectByExample(example);
+		if(list != null && list.size()>0){
+			UUserRRecordB userRecord = list.get(0);
+			Integer type1 = userRecord.getType();
+			if(type <= type1 && type1 < 5){
+				return userRecord;
+			}else{
+				selectGjDlr(userRecord.getrId(),type);
+			}
+		}
+		return null;
+	}
+
+
+	/**
+	 * 计算管理奖的积分方法
+	 * @param id
+	 * @param money
 	 */
 	private void computeJF(int id,double money){
 		//查询积分比例
